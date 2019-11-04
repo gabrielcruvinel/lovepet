20382e6e0192fe8a7494a120b6fc6c7ec11e66b7
gabriel cruvinel <cruvinel.gabriel.gc@gmail.com>
2019-11-04 01:36:44 -0300

    finalizado layout do menu principal

--- frontend/src/pages/Principal/index.js
+++ frontend/src/pages/Principal/index.js
@@ -11,7 +11,32 @@ export default function Principal() {
       <Row>
         <Col span={12} id="principal-infos-esquerdo">
           <h1>lovepet</h1>
+          <Row>
+            <Col id="col-txt-principal">
+              <span class="txt-principal">
+                <h1>A rede social perfeita para o seu animal</h1>
+              </span>
+            </Col>
+            <Col id="principal-grid-info">
+              <span class="text-principal-info">
+                <Icon type="heart-o" /> Encontre um parceiro ideal para seu bichinho
+              </span>
+              <span class="text-principal-info">
+                <Icon type="search" /> Encontre um par ideal
+              </span>
+              <span class="text-principal-info">
+              <Icon type="message" /> Converse com outros donos de pets
+              </span>
+            </Col>
+          </Row>
+          <Row>
+            <Col id="quem-somos">
+              <Button type="primary" icon="question-circle-o">Quem somos</Button>
+            </Col>
+          </Row>
         </Col>
+
+
         <Col span={12} id="principal-info-direito">
           
           <Form layout="inline">
@@ -25,15 +50,19 @@ export default function Principal() {
               <Button type="primary" htmlType="submit" >Entrar</Button>
             </FormItem>
           </Form>
+
           <Row id="row-principal-direito">
             <Col span={24} id="title-principal-direito">
               <h1>Participe dessa comunidade agora</h1>
             </Col>
           </Row>
           <Row>
-            <Col>
+            <Col id="btn-cadastrar">
               <Button type="primary">Cadastrar agora</Button>
             </Col>
+          </Row>
+          <Row>
+
           </Row>
         </Col>
       </Row>
