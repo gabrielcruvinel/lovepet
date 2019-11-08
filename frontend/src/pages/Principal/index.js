4e3dc1c2c544e461028e766f4b8fe0362c000f48
gabriel cruvinel <cruvinel.gabriel.gc@gmail.com>
2019-11-08 12:10:20 -0300

    adicionado uma nova lib e modificado tela de login

--- frontend/src/pages/Principal/index.js
+++ frontend/src/pages/Principal/index.js
@@ -1,70 +1,100 @@
 import React from 'react';
-import './style.css';
-import { Row, Col, Form, Icon, Input, Button } from 'antd';
-import './style.css';
-// import { Container } from './styles';
+import { MdPets, MdFavoriteBorder, MdQuestionAnswer } from "react-icons/md";
+import { Link } from 'react-router-dom'; //REDIRECIONA A PAGINA
+
+import {Container, Preview, TxtPrincipal, PrincipalLogin, BtnCadastro, PrincipalInfo, InfoPrincipalRow } from './styles.js';
 
 export default function Principal() {
-  const FormItem = Form.Item;
-
   return (
-      <Row>
-        <Col span={12} id="principal-infos-esquerdo">
-          <h1>lovepet</h1>
-          <Row>
-            <Col id="col-txt-principal">
-              <span class="txt-principal">
-                <h1>A rede social perfeita para o seu animal</h1>
-              </span>
-            </Col>
-            <Col id="principal-grid-info">
-              <span class="text-principal-info">
-                <Icon type="heart-o" /> Encontre um parceiro ideal para seu bichinho
-              </span>
-              <span class="text-principal-info">
-                <Icon type="search" /> Encontre um par ideal
-              </span>
-              <span class="text-principal-info">
-              <Icon type="message" /> Converse com outros donos de pets
-              </span>
-            </Col>
-          </Row>
-          <Row>
-            <Col id="quem-somos">
-              <Button type="primary" icon="question-circle-o">Quem somos</Button>
-            </Col>
-          </Row>
-        </Col>
+    <Container>
+      <Preview>
+        <TxtPrincipal>
+          <h1>Encontre um parceiro ideal para o seu bichinho agora mesmo</h1>
+        </TxtPrincipal>
+
+        <PrincipalInfo>
+          <InfoPrincipalRow>
+            <MdPets/>Cadastre seu pet
+          </InfoPrincipalRow>
+          <InfoPrincipalRow>
+            <MdFavoriteBorder/>Encontre um parceiro ideal para seu bichinho
+          </InfoPrincipalRow>
+          <InfoPrincipalRow>
+            <MdQuestionAnswer/>Converse com outros donos de pets
+          </InfoPrincipalRow>
+        </PrincipalInfo>
+      </Preview>
+      <PrincipalLogin>
+          <h1>Entrar</h1>
+          <form>
+            <input type="email" placeholder="Digite seu email"></input>
+            <input type="password" placeholder="Digite sua senha"></input>
+            <button type="submit">Entrar</button>
+          </form>
+
+          <BtnCadastro>
+            <Link to="/cadastro">Cadastrar</Link>
+          </BtnCadastro>
+      </PrincipalLogin>
+    </Container>
+
+      // <Row>
+      //   <Col span={12} id="principal-infos-esquerdo">
+      //     <h1>lovepet</h1>
+      //     <Row>
+      //       <Col id="col-txt-principal">
+      //         <span class="txt-principal">
+      //           <h1>A rede social perfeita para o seu animal</h1>
+      //         </span>
+      //       </Col>
+      //       <Col id="principal-grid-info">
+      //         <span class="text-principal-info">
+      //           <Icon type="heart-o" /> Encontre um parceiro ideal para seu bichinho
+      //         </span>
+      //         <span class="text-principal-info">
+      //           <Icon type="search" /> Encontre um par ideal
+      //         </span>
+      //         <span class="text-principal-info">
+      //         <Icon type="message" /> Converse com outros donos de pets
+      //         </span>
+      //       </Col>
+      //     </Row>
+      //     <Row>
+      //       <Col id="quem-somos">
+      //         <Button type="primary" icon="question-circle-o">Quem somos</Button>
+      //       </Col>
+      //     </Row>
+      //   </Col>
 
 
-        <Col span={12} id="principal-info-direito">
+      //   <Col span={12} id="principal-info-direito">
           
-          <Form layout="inline">
-            <FormItem>
-              <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Email" />
-            </FormItem>
-            <FormItem>
-              <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Senha" />
-            </FormItem>
-            <FormItem>
-              <Button type="primary" htmlType="submit" >Entrar</Button>
-            </FormItem>
-          </Form>
+      //     <Form layout="inline">
+      //       <FormItem>
+      //         <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Email" />
+      //       </FormItem>
+      //       <FormItem>
+      //         <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Senha" />
+      //       </FormItem>
+      //       <FormItem>
+      //         <Button type="primary" htmlType="submit" >Entrar</Button>
+      //       </FormItem>
+      //     </Form>
 
-          <Row id="row-principal-direito">
-            <Col span={24} id="title-principal-direito">
-              <h1>Participe dessa comunidade agora</h1>
-            </Col>
-          </Row>
-          <Row>
-            <Col id="btn-cadastrar">
-              <Button type="primary">Cadastrar agora</Button>
-            </Col>
-          </Row>
-          <Row>
+      //     <Row id="row-principal-direito">
+      //       <Col span={24} id="title-principal-direito">
+      //         <h1>Participe dessa comunidade agora</h1>
+      //       </Col>
+      //     </Row>
+      //     <Row>
+      //       <Col id="btn-cadastrar">
+      //         <Button type="primary">Cadastrar agora</Button>
+      //       </Col>
+      //     </Row>
+      //     <Row>
 
-          </Row>
-        </Col>
-      </Row>
+      //     </Row>
+      //   </Col>
+      // </Row>
   );
 }
---
+++ frontend/src/pages/Principal/styles.js
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+export const Container = styled.div`
+    display:flex;
+    flex-direction:row;
+    height: 100vh;
+    background: rgb(44,136,159);
+    background: linear-gradient(128deg, rgba(44,136,159,1) 2%, rgba(240,227,196,0.8253676470588236) 62%);
+
+`;
+
+export const Preview = styled.div `
+    display:flex;
+    flex:1;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+`;
+export const PrincipalInfo = styled.div `
+    display:flex;
+    flex:1;
+    justify-content: center;
+    align-items: center;
+    flex-direction:column;
+
+`;
+
+export const InfoPrincipalRow = styled.span `
+    display:flex;
+    flex:1;
+    margin-left: 12%;
+    padding-top: 30px;
+    font-size: 27px;
+    flex-direction:row;
+    
+
+`;
+
+export const TxtPrincipal = styled.div `
+    text-align:center;
+    font-size: 18px;
+    margin-top:210px;
+`;
+
+export const PrincipalLogin = styled.div`
+    display:flex;
+    flex:1;
+    justify-content:center;
+    align-items:center;
+    flex-direction: column;
+
+    form{
+        display:flex;
+        flex-direction:column;
+        margin-top:30px;
+    }
+
+    input{
+        background: rgba(0, 0, 0 , 0.1);
+        border: 0px;
+        border-radius: 4px;
+        height: 44px;
+        padding: 0 15px;
+        margin: 0 0 10px;
+
+
+        &::placeholder {
+        color: rgba(0, 0, 0, 0.7);
+        }
+    }
+
+    button {
+        margin: 5px 0 0;
+        height: 44px;
+        font-weight: bold;
+        color: #fff;
+        border: 0;
+        border-radius: 4px;
+        background: #248ea9;
+        font-size: 16px;
+        transition: background 0.2s;
+
+        &:hover {
+            background-color:${ darken(0.05, '#248ea9') };
+        }
+    }
+`;
+
+export const BtnCadastro = styled.div`
+    position:fixed;
+    bottom:160px;
+    height: 44px;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    /* justify-content:center;
+    align-items:center;
+    flex-direction: column; */
+    
+`;
